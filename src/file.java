6db1c5ea53aa7c45465a03b8f13ba50acab4f370
basmala1161 <2023170139@cis.asu.edu.eg>
2024-12-05 03:52:54 +0200

    customer data

--- src/file.java
+++ src/file.java
@@ -1,29 +1,22 @@
 import java.io.*;
 import java.util.*;
-
 public class file {
 private static String path;
-
 public file(String path){
     this.path=path;
 }
-
 // READING
-
 public static List <String> read() throws IOException {
     ArrayList<String> data = new ArrayList<>();
-
     BufferedReader reader = new BufferedReader(new FileReader(path));
     String line;
     while ((line = reader.readLine()) != null) {
         data.add(line);
     }
-    reader.close(); // Don't forget to close the reader
+    reader.close();
     return data;
 }
-
 //WHRITING
-
     public static void write(List<String> data) throws IOException{
     BufferedWriter writer=new BufferedWriter(new FileWriter(path));
      for (String record:data){
@@ -32,8 +25,5 @@ public static List <String> read() throws IOException {
      }
 writer.close();
     }
-
-
-
 
 }
